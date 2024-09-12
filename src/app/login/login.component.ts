f18b29351a689b6bc1730c1cca8509dfaa96af59
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-09-12 16:23:11 +0530

    adding initial login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [NgIf,
+    
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+    ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // Handle login logic here
+  }
+}
